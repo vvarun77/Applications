711b347995889eecde97e29250661a179e09f48e
Varun <vvijayababu4334@gmail.com>
2022-01-18 10:38:26 -0800

    asd

---
+++ .vscode/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int x = 12;
+    printf("hello");
+    return 0;
+    int x;
+    for (int i = 1; i <= 10; i++){
+        x++;
+        printf(x);
+    }
+    
+    
+}
